ac93f57cfb1c3bdf4a8bc95f567b65ac0a669a65
Oleg <schika145@gmail.com>
2023-11-07 11:39:47 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу которая найдёт точку пересечения двух прямых, заданых уравнениями y=k1*x + b1, y=k2*x + b2
+// значения b1,k1 и b2,k2 задаются пользователем
+
+
+int[] Point() {
+Console.WriteLine("Введите значения через запятую b1,k1,b2,k2");
+string input = Console.ReadLine();
+string[] arrayStr = input.Split(",");
+int[] nam = new int[arrayStr.Length];
+for (int i = 0; i < arrayStr.Length; i++)
+{
+    nam[i] = Convert.ToInt32(arrayStr[i]);
+} return nam;
+}
+
+void PointCross(int[] array) {
+double X;
+double Y;
+if(array[1] == array[3])
+{ 
+    
+Console.WriteLine("Прямые паралельны, точки пересечения нет");
+
+}
+X = (array[2] - array[0]) / (array[1] - array[3]);
+Y = ((array[1] * array[2]) - (array[3] * array[0])) / (array[1] - array[3]);
+
+Console.WriteLine("Точка пересечения(" +  X  + "," + Y + ")");
+
+}
+
+int[] array = Point();
+PointCross(array);
